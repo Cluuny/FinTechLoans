3d9699076d780120d04b37614ecac09dc90dfab1
Cluuny <vicente.matallana@uptc.edu.co>
2023-05-19 11:17:29 -0500

    Merge remote <-> loca

--- src/main/java/com/fintechloans/view/View.java
+++ src/main/java/com/fintechloans/view/View.java
@@ -1,24 +1,38 @@
 package com.fintechloans.view;
 
 import java.util.Scanner;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 
 public class View {
   private Scanner sc;
 
-  public View() {
-    sc = new Scanner(System.in);
-  }
+  // public View() {
+  // // sc = new Scanner(System.in);
+  // }
 
   public int readInt(String message) {
     System.out.println(message);
-    return sc.nextInt();
+    int input = 0;
+    try {
+      sc = new Scanner(System.in);
+      input = sc.nextInt();
+      return input;
+    } catch (Exception e) {
+      System.out.println("Error de tipo: " + e.getMessage() + " porfavor ingrese un valor numérico valido.");
+      return this.readInt(message);
+    }
   }
 
   public String readString(String message) {
     System.out.println(message);
-    return sc.next();
+    String input = "";
+    try {
+      sc = new Scanner(System.in);
+      input = sc.nextLine();
+      return input;
+    } catch (Exception e) {
+      System.out.println("Error de tipo: " + e.getMessage() + " porfavor ingrese una cadena de caracteres valida.");
+      return this.readString(message);
+    }
   }
 
   public void print(String message) {
